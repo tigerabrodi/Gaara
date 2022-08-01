0e15c16d4de858a338b7403ac6e71d9e255fd62c
tigerabrodi <tigerabrodi@gmail.com>
2022-08-01 06:16:38 +0200

    setup eslint, prettier, and some clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import type { Component } from 'solid-js';
-
-const App: Component = () => {
+export const App = () => {
   return (
     <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-  );
-};
-
-export default App;
+  )
+}
