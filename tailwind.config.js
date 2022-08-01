8c469efcf4b6b14b849ce03b730766993e804ac7
tigerabrodi <tigerabrodi@gmail.com>
2022-08-01 06:50:18 +0200

    chore: setup tailwind colors and tryna fix typings but aint working meh

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,css,md,mdx,html,json,scss}',
   ],
-  darkMode: "class",
+  darkMode: 'class',
   theme: {
     extend: {},
+    colors: {
+      blue: 'rgb(29, 155, 240)',
+      navy: 'rgba(21,32,43,1.00)',
+      gray: {
+        300: 'rgb(66, 83, 100)',
+        600: 'rgb(56, 68, 77)',
+      },
+      white: 'rgb(247, 249, 249)',
+    },
   },
   plugins: [],
-};
+}
