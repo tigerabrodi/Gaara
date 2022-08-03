71046f798da2c013a93ec91ffcae23694a5c7318
tigerabrodi <tigerabrodi@gmail.com>
2022-08-03 06:12:41 +0200

    feat(ProgressCircleIcon): implement colors as constants

--- src/icons/ProgressCircleIcon.tsx
+++ src/icons/ProgressCircleIcon.tsx
@@ -1,4 +1,17 @@
-export const ProgressCircleIcon = (props: { class: string }) => (
+const STROKE_DASH_OFFSET = 56.3467
+
+const STROKE_SUCCESS_COLOR = '#1D9BF0'
+
+const STROKE_WARNING_COLOR = '#FFD400'
+
+const STROKE_ERROR_COLOR = '#F4212E'
+
+const STROKE_BACKGROUND_COLOR = '#38444D'
+
+export const ProgressCircleIcon = (props: {
+  class: string
+  strokeDashArray: number
+}) => (
   <svg
     height="100%"
     viewBox="0 0 20 20"
@@ -12,7 +25,7 @@ export const ProgressCircleIcon = (props: { class: string }) => (
       fill="none"
       stroke-width="2"
       r="9"
-      stroke="#38444D"
+      stroke={STROKE_BACKGROUND_COLOR}
     ></circle>
     <circle
       cx="50%"
@@ -20,9 +33,9 @@ export const ProgressCircleIcon = (props: { class: string }) => (
       fill="none"
       stroke-width="2"
       r="9"
-      stroke="#1D9BF0"
+      stroke={STROKE_SUCCESS_COLOR}
       stroke-linecap="round"
-      style="stroke-dashoffset: 56.3467; stroke-dasharray: 56.5487;"
+      style={`stroke-dashoffset: ${STROKE_DASH_OFFSET}; stroke-dasharray: ${props.strokeDashArray};`}
     ></circle>
   </svg>
 )
