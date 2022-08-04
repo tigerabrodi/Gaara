e4966809bef45ad1cc32b73682d58706beccd859
tigerabrodi <tigerabrodi@gmail.com>
2022-08-04 06:20:59 +0200

    feat(ProgressCircleIcon): implement reactive validation for the colors

--- src/icons/ProgressCircleIcon.tsx
+++ src/icons/ProgressCircleIcon.tsx
@@ -1,3 +1,6 @@
+import { createEffect, createSignal, mergeProps } from 'solid-js'
+import { MAX_CHARACTERS_LENGTH } from '../App'
+
 const STROKE_DASH_OFFSET = 56.3467
 
 const STROKE_SUCCESS_COLOR = '#1D9BF0'
@@ -8,34 +11,51 @@ const STROKE_ERROR_COLOR = '#F4212E'
 
 const STROKE_BACKGROUND_COLOR = '#38444D'
 
+const SUCCESS_CHARACTERS_LENGTH = 260
+
 export const ProgressCircleIcon = (props: {
   class: string
   strokeDashArray: number
-}) => (
-  <svg
-    height="100%"
-    viewBox="0 0 20 20"
-    width="100%"
-    style="overflow: visible;"
-    class={props.class}
-  >
-    <circle
-      cx="50%"
-      cy="50%"
-      fill="none"
-      stroke-width="2"
-      r="9"
-      stroke={STROKE_BACKGROUND_COLOR}
-    ></circle>
-    <circle
-      cx="50%"
-      cy="50%"
-      fill="none"
-      stroke-width="2"
-      r="9"
-      stroke={STROKE_SUCCESS_COLOR}
-      stroke-linecap="round"
-      style={`stroke-dashoffset: ${STROKE_DASH_OFFSET}; stroke-dasharray: ${props.strokeDashArray};`}
-    ></circle>
-  </svg>
-)
+  tweetValue: string
+}) => {
+  const shouldShowSuccessColor = () =>
+    props.tweetValue.length < SUCCESS_CHARACTERS_LENGTH
+  const shouldShowWarningColor = () =>
+    props.tweetValue.length < MAX_CHARACTERS_LENGTH
+
+  const strokeColor = () =>
+    shouldShowSuccessColor()
+      ? STROKE_SUCCESS_COLOR
+      : shouldShowWarningColor()
+      ? STROKE_WARNING_COLOR
+      : STROKE_ERROR_COLOR
+
+  return (
+    <svg
+      height="100%"
+      viewBox="0 0 20 20"
+      width="100%"
+      style="overflow: visible;"
+      class={props.class}
+    >
+      <circle
+        cx="50%"
+        cy="50%"
+        fill="none"
+        stroke-width="2"
+        r="9"
+        stroke={STROKE_BACKGROUND_COLOR}
+      ></circle>
+      <circle
+        cx="50%"
+        cy="50%"
+        fill="none"
+        stroke-width="2"
+        r="9"
+        stroke={strokeColor()}
+        stroke-linecap="round"
+        style={`stroke-dashoffset: ${STROKE_DASH_OFFSET}; stroke-dasharray: ${props.strokeDashArray};`}
+      ></circle>
+    </svg>
+  )
+}
