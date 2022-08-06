36982426246bc991bd0f6c9220e5e36e6d4cc971
tigerabrodi <tigerabrodi@gmail.com>
2022-08-06 16:28:51 +0200

    change title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { tweets } from './store'
 export const App = () => {
   return (
     <main class="flex min-h-full w-full flex-col items-center bg-white">
-      <h1 class="mt-12 text-6xl text-navy">Twitter Dialog UI</h1>
+      <h1 class="mt-12 text-6xl text-navy">Gaara</h1>
 
       <div class="relative mt-8 min-h-[280px] w-[600px] rounded-xl bg-navy p-4 pt-5 shadow-sm shadow-gray-600">
         <For each={tweets}>{(tweet) => <TweetInput tweet={tweet} />}</For>
